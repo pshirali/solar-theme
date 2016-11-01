0c0694e44a3816836d0732398ea33799c5af9a39
Praveen Shirali <praveengshirali@gmail.com>
2016-11-01 17:43:01 +0530

    Bumped the version to 1.3.3

--- solar_theme/__init__.py
+++ solar_theme/__init__.py
@@ -5,7 +5,7 @@ import os.path
 
 __author__ = 'Vimalkumar Velayudhan'
 __email__ = 'vimalkumarvelayudhan@gmail.com'
-__version__ = '1.3.2'
+__version__ = '1.3.3'
 
 pkg_dir = os.path.abspath(os.path.dirname(__file__))
 theme_path = os.path.split(pkg_dir)[0]
